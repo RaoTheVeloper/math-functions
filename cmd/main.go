8cfebd83634e18f711a17226813c03349cb4c59b
RaoTheVeloper <raotheveloper@gmail.com>
2023-02-19 17:16:53 +0100

    gcd example

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/RaoTheVeloper/math-functions/pkg"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	fmt.Printf("GCD(%d, %d) = %d", 10, 5, pkg.GCD(10, 5))
 }
